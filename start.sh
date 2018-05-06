8a1ca1ef101307c0f9524038ac3035bbc219d449
Ryan Noelk <ryannoelk@gmail.com>
2018-05-06 09:11:32 -0400

    removing NODE_PORT env var and using location.origin as the default apiHost

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # NGINX doesn't let you use ENV vars within the conf file.
 # We use sed to set the env vars we need.
-# Sed replace the API_PORT and NODE_PORT vars.
+# Sed replace the API_PORT vars.
 
 sed -i "s/API_PORT/$API_PORT/g" /etc/nginx/conf.d/default.conf;
 
